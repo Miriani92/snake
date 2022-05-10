66befb38a0047c2d256b763490077c6b9016f0b8
miriani <mirian.tsintsadze1992@gmail.com>
2022-05-10 19:16:51 +0400

    css

--- src/component/Board.js
+++ src/component/Board.js
@@ -1,44 +1,9 @@
-import React, { useEffect, useState } from "react";
-import useFood from "./useFood";
+import React, { useState } from "react";
 
-const width = 20;
 const Board = () => {
-  const [squares, setSquares] = useState([]);
-  const [snakePostion, setSnakePostion] = useState(0);
-  const [foodPosition, setFoodPostion] = useState();
-  const foodIndex = useFood();
-  const createBoard = () => {
-    let squeare = [];
-    for (let i = 0; i < 400; i++) {
-      squeare.push(i);
-    }
-    return setSquares([...squeare]);
-  };
-
-  useEffect(() => {
-    createBoard();
-  }, []);
-
-  useEffect(() => {
-    setFoodPostion(foodIndex);
-  }, [useFood]);
-
   return (
     <div className="board">
-      {squares.map((_, index) => {
-        return (
-          <div
-            key={index}
-            className={`square ${
-              index === snakePostion
-                ? "snake"
-                : "" || index === foodPosition
-                ? "food"
-                : ""
-            }`}
-          ></div>
-        );
-      })}
+      <h1>hello world</h1>
     </div>
   );
 };
--- src/component/useFood.js
+++
@@ -1,6 +0,0 @@
-const useFood = () => {
-  const randomNumber = Math.floor(Math.random() * 399);
-  return randomNumber;
-};
-
-export default useFood;
